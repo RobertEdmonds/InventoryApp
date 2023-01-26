bab9f45bbea2ebdd5fac24b26eac6343cfdc551a
Robert Edmonds <bobby.edmonds89@gmail.com>
2023-01-26 13:10:18 -0500

    Check

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :samples
-  resources :products
-  resources :orders
-  resources :employees
+  resources :samples, only: [:create, :index, :destroy, :update, :show]
+  resources :products, only: [:create, :index, :destroy, :update, :show]
+  resources :orders, only: [:create, :index, :destroy, :update, :show]
+  # resources :employees
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
