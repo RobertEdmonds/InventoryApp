b470cb2fb750d217a4f997f2e690d31c1b46d1ad
Robert Edmonds <bobby.edmonds89@gmail.com>
2023-02-02 14:40:01 -0500

    Add routes to employee and application controllers

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,55 @@
 class EmployeesController < ApplicationController
+    skip_before_action :authorize, only: [:create, :index]
+    before_action :authorize_user, only: [:update_employee] 
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity 
+
+    def index 
+        render json: Employee.all, status: :ok 
+    end
+
+    def create
+        if employee.amount_of_logins == 0
+            employee = Employee.create!(employee_params)
+        else
+            employee = Employee.update!(employee_params)
+            session[:employee_id] = employee.id 
+        end
+        render json: employee, status: :created 
+    end
+
+    def show 
+        current_user = Employee.find(session[:employee_id])
+        render json: current_user
+    end
+
+    def update
+        employee = Employee.find(session[:employee_id])
+        employee.update(employee_params)
+        render json: employee, status: :created 
+    end
+
+    def update_employee
+        employee = Employee.find(params[:id])
+        employee.update(update_employee_params)
+        render json: employee, status: :created 
+    end
+
+    private 
+
+    def employee_params 
+        params.permit(:name, :phone_number)
+    end
+
+    def update_employee_params 
+        params.permit(:boss)
+    end
+
+    def authorize_user
+        user_can_see = current_user.employee?
+        render json: { error: "Ah ah ah, you didn't say the magic word" }, status: :forbidden unless user_can_see
+    end
+
+    def render_unprocessable_entity(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity 
+    end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,14 @@
 class Employee < ApplicationRecord
     has_secure_password
+
+    PASSWORD_REQUIREMENTS = /\A
+        (?=.{8,}) # At least 8 characters long
+        (?=.*\d) # Contain at least one number
+        (?=.*[a-z]) # Contain at least one lowercase letter
+        (?=.*[A-Z]) # Contain at least one uppercase letter
+        (?=.*[[:^alnum:]]) # Contain at least one symbol
+        /x
+
     has_many :orders 
     has_many :rented_samples
     has_many :products, through: :orders
@@ -7,4 +16,5 @@ class Employee < ApplicationRecord
 
     validates :name, presence: true, uniqueness: true 
     validates :phone_number, presence: true, uniqueness: true 
+    validates :password, format: PASSWORD_REQUIREMENTS, confirmation: true, unless: {|employee| employee.amount_of_logins == 0} 
 end
