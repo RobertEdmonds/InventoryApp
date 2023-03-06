e2fe849bbd4672e379a7cca76e8008c3e7fd17c0
Robert Edmonds <bobby.edmonds89@gmail.com>
2023-03-06 15:42:34 -0500

    Add routes in picture controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,24 @@
 class PicturesController < ApplicationController
+    skip_before_action :authorize, only: [:index]
+
+    def index 
+        render json: Picture.all, status: :ok
+    end
+
+    def create 
+        picture = Picture.create!(picture_params)
+        render json: picture, status: :created
+    end
+
+    def destroy
+        picture = Picture.find(params[:id])
+        picture.destroy
+        head :no_content
+    end
+
+    private 
+
+    def picture_params 
+        params.permit(:picture_url, :product_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :rented_samples
-  resources :pictures
+  resources :pictures, only: [:index, :create, :destroy]
   resources :samples, only: [:create, :index, :destroy, :update, :show]
   resources :products, only: [:create, :index, :destroy, :update, :show]
-  resources :orders, only: [:create, :index, :destroy, :update, :show]
+  resources :orders
   # resources :employees
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
