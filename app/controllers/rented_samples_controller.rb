be82358bf36e4bc1ab591a564627afa67072c5c7
Robert Edmonds <bobby.edmonds89@gmail.com>
2023-03-13 17:57:43 -0400

    Add more routes to RentedSamples table

--- app/controllers/rented_samples_controller.rb
+++ app/controllers/rented_samples_controller.rb
@@ -5,6 +5,15 @@ class RentedSamplesController < ApplicationController
         render json: RentedSample.all, status: :ok
     end
 
+    def show
+        rented_samples = RentedSample.find(params[:id])
+        render json: rented_samples, status: :ok
+    end
+
+    def create 
+        rs = RentedSample.create!(rented_samples_params)
+        render json: rs, status: :created
+    end
     private
 
     def rented_samples_params 
