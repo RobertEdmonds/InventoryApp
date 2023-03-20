d2d8bd44d31ffd9cd736cefea527ace1717881a6
Robert Edmonds <bobby.edmonds89@gmail.com>
2023-03-20 13:11:14 -0400

    Add content to login

--- client/src/forms/Login.js
+++ client/src/forms/Login.js
@@ -1,6 +1,74 @@
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { EmployeeContext } from "../context/Employee.js";
+
+
 function Login(){
+    const { setEmployee } = useContext(EmployeeContext);
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState([])
+    const history = useNavigate()
+    
+    function handleSubmit(e) {
+        e.preventDefault();
+        setLoading(false);
+        const dataForm = {
+          username,
+          password,
+        };
+        fetch("/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(dataForm),
+        }).then((r) => {
+          setLoading(false);
+          if (r.ok) {
+            r.json().then((user) => {
+              setEmployee(user)
+              history.push("/");
+            });
+            setUsername("");
+            setPassword("");
+          } else {
+            r.json().then((err) => setError(err.errors));
+          }
+        });
+      }
     return(
-        <div>Login</div>
+        <>
+            <ul className="errorStyle">
+            {error.map((err) => {
+            return <li key={err}>{err}</li>;
+            })}
+            </ul>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="formGroupExampleInput" className="form-label">Example label</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="formGroupExampleInput" 
+                        placeholder="Example input placeholder" 
+                        value={username}
+                        onChange={e => setUsername(e.target.value.trim())} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="formGroupExampleInput2" className="form-label">Another label</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="formGroupExampleInput2" 
+                        placeholder="Another input placeholder"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}/>
+                </div>
+            </form>
+            <button onClick={() => setLoading(!loading)}></button>
+        </>
     )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './styles/index.css';
 import App from './component/App';
+import { EmployeeProvider } from './context/Employee';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <EmployeeProvider>
+      <App />
+    </EmployeeProvider>
   </BrowserRouter>
 );
 
