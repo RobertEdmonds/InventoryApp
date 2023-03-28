33953bcf400735eeaf68910ec849abb63b315617
Robert Edmonds <bobby.edmonds89@gmail.com>
2023-03-28 16:12:10 -0400

    Design for clickable modal on ShowProduct

--- client/src/component/ShowProduct.js
+++ client/src/component/ShowProduct.js
@@ -15,7 +15,21 @@ function ShowProduct({product, productId}){
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
-                        ...
+                        <div className="card" style={{width: ""}} >
+                            <div className="container text-center">
+                                <div className="row">
+                                    <div className="col">
+                                        <img src={tile.pictures[0].picture_url} className="card-img-top" alt="tile"/>
+                                    </div>
+                                    <div className="col">
+                                        <h5 className="card-title">{tile.name}</h5>
+                                        <h6 className="card-title">${tile.cost}</h6>
+                                        <p className="card-text">{tile.description}</p>
+                                        <p className="card-text">{tile.inventory}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
