bb3b9178d9445e8d6df560aeeeabbc78e59c797f
Robert Edmonds <bobby.edmonds89@gmail.com>
2023-03-31 22:48:58 -0400

    Add button to set customer context

--- client/src/component/CustPage.js
+++ client/src/component/CustPage.js
@@ -1,6 +1,9 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useContext } from "react";
+import {CustomerContext} from "../context/Customer.js";
+
 
 function CustPage(){
+    const {customer, setCustomer} = useContext(CustomerContext)
     const [ custList, setCustList ] = useState([])
     const [value, setValue] = useState('')
 
@@ -12,20 +15,17 @@ function CustPage(){
             }
         })
     },[])
-    // console.log(custList)
+
     const filterCustomer = custList.filter((cust) => {
-        console.log("value", value)
-        // if (value === "All") return true;
         if(value.length < cust.name.length) {
             let custSplit = cust.name.slice(0,value.length)
-            console.log("slice",cust.name.slice(0,value.length))
             return value.toUpperCase() === custSplit
         }else{
             console.log("else", value.toUpperCase() === cust)
             return value.toUpperCase() === cust.name
         }
       });
-    console.log(filterCustomer)
+    console.log(customer)
     return(
         <div>
             <div className="mb-3">
@@ -44,7 +44,7 @@ function CustPage(){
             {filterCustomer.map(cust => {
                 return(
                     <div className="col" key={cust.id}>
-                        <div className="card" style={{width: "18rem;"}}>
+                        <div className="card" style={{width: "18rem"}}>
                             <div className="card-body">
                                 <h5 className="card-title">{cust.name}</h5>
                                 <h6 className="card-subtitle mb-2 text-body-secondary">{cust.phone_number}</h6>
@@ -52,6 +52,7 @@ function CustPage(){
                                 <p className="card-text">{cust.address}<br/>{cust.city}, {cust.state} {cust.zip_code}</p>
                                 <button className="card-link btn btn-primary">Samples</button>
                                 <button className="card-link btn btn-primary">Orders</button>
+                                <button className="card-link btn btn-primary" onClick={() => setCustomer(cust)}>Set Customer</button>
                             </div>
                         </div>
                     </div>
